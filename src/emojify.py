6452ba90465f912922120d8b89b65ff5c3e3f73a
daryl <darylleesy@gmail.com>
2022-03-12 19:59:58 +1100

    Implemented emojify command

---
+++ src/emojify.py
@@ -0,0 +1,13 @@
+import requests
+import json
+
+URL = 'https://api.emojify.net/convert'
+
+def emojify(text, density=50):
+    payload = {
+        'density' : density,
+        'input' : text,
+        'shouldFilterEmojis' : False
+        }
+    resp = requests.post(URL, json=payload)
+    return json.loads(resp.text)['result'].replace(u'\u200b', ' ')
